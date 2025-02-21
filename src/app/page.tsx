b4caa7a2056cd421607bcc3779801c2de6684c5a
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2025-02-21 23:37:44 +0600

    set up clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { ThemeToggler } from "@/components/theme-toggler";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
