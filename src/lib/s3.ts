7a913f69fb8508e8b0d732809cc7b675c6b2b661
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2025-02-25 08:46:30 +0600

    fixed some s3 bucket issues

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -3,21 +3,17 @@ import AWS from "aws-sdk";
 export async function uploadToS3(file: File) {
   try {
     AWS.config.update({
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    });
-    const s3 = new AWS.S3({
-      params: {
-        Bucket: process.env.S3_BUCKET_NAME,
-      },
+      accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
       region: "eu-north-1",
     });
+    const s3 = new AWS.S3();
 
     const file_key =
       "uploads/" + Date.now().toString() + file.name.replace(" ", "-");
 
     const params = {
-      Bucket: process.env.S3_BUCKET_NAME!,
+      Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
       Key: file_key,
       Body: file,
     };
